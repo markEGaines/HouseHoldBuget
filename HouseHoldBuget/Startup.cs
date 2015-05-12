417d6a620fe371eeb6060be19832a14e0cfa0615
markEGaines <markegaines@gmail.com>
2015-05-12 16:07:19 -0400

    initial commit

---
+++ HouseHoldBuget/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HouseHoldBuget.Startup))]
+namespace HouseHoldBuget
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
