8bda424b23b19b8faf6cce3d557cbe67b0a1313d
markEGaines <markegaines@gmail.com>
2015-06-16 13:15:10 -0400

    fixed some buttons - added guest login

--- HouseHoldBuget/Controllers/AccountController.cs
+++ HouseHoldBuget/Controllers/AccountController.cs
@@ -92,6 +92,39 @@ namespace HouseHoldBuget.Controllers
         }
 
         //
+        // POST: /Account/Login  GUEST LOGIN
+      //  [HttpPost]
+        [AllowAnonymous]
+      //  [ValidateAntiForgeryToken]
+        public async Task<ActionResult> GuestLogin()
+        {
+            var model = new LoginViewModel
+            {
+                Email = "Guest@CoderFoundry.com",
+                Password = "Password-1",
+                RememberMe = false,
+            };
+            var returnUrl = "/Home/Index";
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
+        //
         // GET: /Account/VerifyCode
         [AllowAnonymous]
         public async Task<ActionResult> VerifyCode(string provider, string returnUrl, bool rememberMe)
